5f1cdc02964b172c56a82a7f72ed6569751f4e1e
Gian Ferreira <56806058+gianferreira@users.noreply.github.com>
2020-06-10 01:37:32 -0300

    : Finalização Email
    Finalização de funcionalidade de envio de email e criação de template.

--- src/modules/users/services/SendForgotPassWordEmailService.ts
+++ src/modules/users/services/SendForgotPassWordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -30,12 +31,29 @@ class SendForgotPassWordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido',
+    const forhotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
     );
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forhotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
